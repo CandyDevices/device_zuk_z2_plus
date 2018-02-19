cc39b8bb2d1fd295869adb02b6a24b4270ac6a9a
Mourya Baruah <mouryabaruah@gmail.com>
2018-02-19 10:04:36 +0000

    Dot -ify

--- dot.mk
+++ dot.mk
@@ -24,9 +24,9 @@ $(call inherit-product, device/zuk/z2_plus/device.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit some common AOSP-OMS stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
-PRODUCT_NAME := aosp_z2_plus
+PRODUCT_NAME := dot_z2_plus
 PRODUCT_DEVICE := z2_plus
 PRODUCT_MANUFACTURER := ZUK
 PRODUCT_BRAND := ZUK
